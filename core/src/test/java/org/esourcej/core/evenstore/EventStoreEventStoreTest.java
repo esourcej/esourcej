11fc7e0d3f35278c30aace79a93ef5bdee0cdd8b
Slawomir Laskowski <laskowski.box@gmail.com>
2017-12-30 02:48:52 +0100

    Fix eventstore host

--- core/src/test/java/org/esourcej/core/evenstore/EventStoreEventStoreTest.java
+++ core/src/test/java/org/esourcej/core/evenstore/EventStoreEventStoreTest.java
@@ -2,23 +2,31 @@ package org.esourcej.core.evenstore;
 
 import com.github.msemys.esjc.*;
 import com.github.msemys.esjc.EventStore;
-import org.esourcej.core.evenstore.exception.VersionMismatchException;
-import org.esourcej.core.event.Event;
 import org.esourcej.core.event.serializer.InMemoryEventSerializer;
 import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class EventStoreEventStoreTest extends EventStoreTest {
+    private String EVENTSTORE_HOST = "ESOURCEJ_CORE_TEST_EVENTSTORE_TEST";
+
     @Before
     public void setUpEventStore() throws Exception {
+
+        String host = getHost();
+
         EventStore esjcEventStore = EventStoreBuilder.newBuilder()
-                .singleNodeAddress("eventstore", 1113)
+                .singleNodeAddress(host, 1113)
                 .userCredentials("admin", "changeit")
                 .build();
 
         eventStore = new EventStoreEventStore(esjcEventStore, new InMemoryEventSerializer());
+    }
+
+    private String getHost() {
+        String host = System.getenv(EVENTSTORE_HOST);
+
+        host = host == null ? "localhost" : host;
+
+        return host;
     }
 
 }
